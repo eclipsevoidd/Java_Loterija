c90ea606ccb2d75b72fd528dd66e85efa9203e1a
eclipsevoidd <gustavs.lacis@sk.lvt.lv>
2025-12-16 09:13:36 +0200

    mainīgo un stacka definēšana

--- src/loterija/Loterija.java
+++ src/loterija/Loterija.java
@@ -1,9 +1,24 @@
 package loterija;
 
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.Stack;
+
 public class Loterija {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        
+        Stack<Integer> laimigieSkaitli = new Stack<>();
+        LocalDateTime pedejaIzloze = null;
+        int gaidisanasLaiks = 1;
 
-	}
-
+        Stack<Integer> bumbinuSteks = new Stack<>();
+        
+        String izvele;
+        String[] darbibas = {"Veikt jaunu izlozi", "Apskatīt laimīgos skaitļus un datumu", "Apturēt"};
+        
+        do {
+           
+        } while (!izvele.equals("Apturēt"));
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module LoterijasProjekts {
+	requires java.desktop;
 }
